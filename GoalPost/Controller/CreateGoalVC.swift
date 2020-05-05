5f60ddb429caec26c24ce921f67c8162f9afe05a
Mariah Baysic <mjpb123@gmail.com>
2020-05-05 15:45:37 +0800

    Main.storyboard: added CreateGoalVC objects; CreateGoalVC.swift: (new) used for CreateGoalVC - added IBOutlets and IBActions

---
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -0,0 +1,35 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Mariah Baysic on 5/5/20.
+//  Copyright © 2020 SpacedOut. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTxtView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
