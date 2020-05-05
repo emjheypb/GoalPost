255bc2c220cffbf8a107459c324d10be8301fc15
Mariah Baysic <mjpb123@gmail.com>
2020-05-05 17:08:33 +0800

    Main.storyboard: modified next button bottom constraint; CreateGoalVC.swift: added next button binding, added actions to short and long term buttons; UIButtonExt.swift: (new) sets button background color when selected and deselected; UIViewExt.swift: (new) binds objects to keyboard

--- GoalPost/Controller/CreateGoalVC.swift
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -15,17 +15,30 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    var goalType : GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nextBtn.bindToKeyboard()
+        
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
 
     @IBAction func nextBtnPressed(_ sender: Any) {
     }
     
     @IBAction func shortTermBtnPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
     
     @IBAction func longTermBtnPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermBtn.setSelectedColor()
+        shortTermBtn.setDeselectedColor()
     }
     
     @IBAction func backBtnPressed(_ sender: Any) {
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Goal tracker app - data persistence with Core Data
 
 - core data (tick Core Data checkbox upon project creation)
+- change present view controller animation to slide from right to left and vice versa using UIViewController extension (UIViewControllerExt.swift, GoalsVC.swift, CreateGoalVC.swift)
+- bind button to keyboard using UIView extension (UIViewExt.swift)
+- change button color on select and deselect using UIButton extension (UIButtonExt.swift)
